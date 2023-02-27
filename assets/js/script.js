fe708c5c2b45702461874e4495d0f6a9b9039b93
Agshinhummatov <agshinshh@code.edu.az>
2023-02-28 03:25:22 +0400

    homework

---
+++ assets/js/script.js
@@ -0,0 +1,54 @@
+$(document).ready(function(){
+
+
+    // let elem = $("h1")
+    // console.log(elem.text("sagollar"));
+
+    // elem.html("<span>salam</span>")
+
+    // $("button").click(function(){
+    //     $(this).css("background-color","red")
+    // })
+
+    // $(".check").click(function(){
+    //     $(this).css("background-color","red")
+    // })
+
+    // $(".add").click(function(){
+    //     $(this).css("background-color","magenta");
+    //     $(this).next().css("background-color","magenta");
+    // })
+
+    // let elem = $("h1")
+
+    // $(".hide").click(function(){
+    //     $(".content").hide(2000);
+    // })
+
+    // // $(".show").click(function(){
+    // //     $(".content").show(2000);
+    // // })
+
+    // $(".toggle").click(function(){
+    //     $(".content").toggle(2000);
+    // })
+
+    // $(".show").click(function(){
+    //     $(".content").slideDown(2000,function(){
+    //         $(this).css("background-color","blue")
+    //     });
+    // })
+
+
+    $('#sidebar-btn').on('click', function () {
+        $('#sidebar').toggleClass('visible');
+    });
+
+
+
+
+
+});
+
+
+
